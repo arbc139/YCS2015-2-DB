3de724617471c2a70ab4547c24f8569aa8c792ae
Olaf Ahn <adoji92@gmail.com>
2015-11-05 10:03:37 +0900

    Highlighting all navbar items including (sign-in, sign-up)

--- front/app/app.js
+++ front/app/app.js
@@ -2,26 +2,33 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.sign',
-  'myApp.version'
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.sign',
+    'myApp.version'
 ])
-  .config(['$routeProvider', function($routeProvider) {
+.config(['$routeProvider', function($routeProvider) {
     $routeProvider.otherwise({redirectTo: '/view1'});
-  }])
-  .constant("navbarActiveClass", "active")
-  .controller("navListCtrl", function ($scope, navbarActiveClass) {
+}])
+.constant("navbarActiveClass", "active")
+.controller("navListCtrl", function ($scope, navbarActiveClass) {
     var selectedNav = null;
 
     $scope.navList = ["view1", "view2"];
+    $scope.signList = [{
+        name: "SIGN IN",
+        url : "#/sign-in"
+    }, {
+        name: "SIGN UP",
+        url: "#/sign-up"
+    }];
 
     $scope.selectNav = function(newNav) {
-      selectedNav = newNav;
+        selectedNav = newNav;
     };
 
     $scope.getNavClass = function (nav) {
-      return selectedNav === nav ? navbarActiveClass : "";
+        return selectedNav === nav ? navbarActiveClass : "";
     };
-  });
+});
