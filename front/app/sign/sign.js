93aaa77872df0f055033b8c0efed608b163013cf
Olaf Ahn <adoji92@gmail.com>
2015-11-17 13:01:24 +0900

    Reindent

--- front/app/sign/sign.js
+++ front/app/sign/sign.js
@@ -1,42 +1,52 @@
 'use strict';
 
 angular.module('myApp.sign', ['ngRoute'])
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/sign-in', {
-            templateUrl: 'sign/sign-in.html',
-            controller: 'signInCtrl'
+// .factory("Secure", function($resource) {
+//     return $resource("/api/record/:id:password", { id: "@id" },
+//     {
+//         'show':    { method: 'GET', isArray: false },
+//         'update':  { method: 'PUT' },
+//         'destroy': { method: 'DELETE' }
+//     }
+//     );
+// })
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/sign-in', {
+        templateUrl: 'sign/sign-in.html',
+        controller: 'signInCtrl'
+    });
+}])
+.controller('signInCtrl', function($scope, $http) {
+    $scope.signInUserModel = {
+        'id': '',
+        'password': ''
+    };
+
+    $scope.signIn = function(id, password) {
+        $http
+        .post("http://ror.olaf.kr/login") // todo ask url to dy
+        .success(function (data) {
+            // $scope.signInUserModel.id = "success";
+            // $scope.signInUserModel.password = "success!!";
+            console.log(data);
+
+        })
+        .error(function (data) {
+            $scope.signInUserModel.id = "error";
+            $scope.signInUserModel.password = "error!!";
+
+            console.log('??');
+            console.log(data);
         });
-    }])
-    .controller('signInCtrl', function($scope, $http) {
-        $scope.signInUserModel = {
-            'id': '',
-            'password': ''
-        };
+    };
+})
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/sign-up', {
+        templateUrl: 'sign/sign-up.html',
+        controller: 'signUpCtrl'
+    });
+}])
+.controller('signUpCtrl', [function($scope) {
 
-        $scope.signIn = function(id, password) {
-            $http
-            .post("http://localhost:3000/login") // todo ask url to dy
-            .success(function (data) {
-                $scope.signInUserModel.id = "success";
-                $scope.signInUserModel.password = "success!!";
-
-            })
-            .error(function (data) {
-                $scope.signInUserModel.id = "error";
-                $scope.signInUserModel.password = "error!!";
-
-                console.log('??');
-                console.log(data);
-            });
-        };
-    })
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/sign-up', {
-            templateUrl: 'sign/sign-up.html',
-            controller: 'signUpCtrl'
-        });
-    }])
-    .controller('signUpCtrl', [function($scope) {
-
-    }])
-    ;
+}])
+;
