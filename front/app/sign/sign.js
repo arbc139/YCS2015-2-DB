cd15903c4eda25dc95a5002395393deae0cf15ae
Olaf Ahn <adoji92@gmail.com>
2015-11-17 11:07:40 +0900

    sign in WIP

--- front/app/sign/sign.js
+++ front/app/sign/sign.js
@@ -7,9 +7,24 @@ angular.module('myApp.sign', ['ngRoute'])
             controller: 'signInCtrl'
         });
     }])
-    .controller('signInCtrl', [function($scope) {
+    .controller('signInCtrl', function($scope, $http) {
+        $scope.signInUserModel = {
+            'id': '',
+            'password': ''
+        };
 
-    }])
+        $scope.signIn = function(id, password) {
+            $http
+            .post("127.0.0.1:3000/sign-in") // todo ask url to dy
+            .success(function (data) {
+
+            })
+            .error(function (data) {
+                id = 'something went wrong!';
+                password = 'sibal!';
+            });
+        };
+    })
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/sign-up', {
             templateUrl: 'sign/sign-up.html',
