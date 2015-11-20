faff554740c656620be46bf0f332e7965f6fed49
Olaf Ahn <adoji92@gmail.com>
2015-11-20 14:09:38 +0900

    Create signin & siginup Ctrl

---
+++ td2/app/scripts/controllers/signin.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name td2App.controller:SigninCtrl
+ * @description
+ * # SigninCtrl
+ * Controller of the td2App
+ */
+angular.module('td2App')
+  .controller('SigninCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
