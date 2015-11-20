45bdf162d9b759b9dbad5613573076278e553d9a
Olaf Ahn <adoji92@gmail.com>
2015-11-20 14:04:06 +0900

    admin page

--- front/app/myPage/adminPage.js
+++ front/app/myPage/adminPage.js
@@ -7,10 +7,21 @@ angular.module('myApp.adminPage', ['ngRoute'])
   });
 }])
 
-.controller('adminPageCtrl', function($location, SessionService, SESSION_TYPE) {
+.controller('adminPageCtrl', function($scope, $location, SessionService, SESSION_TYPE) {
+    // check admin!
     if (SessionService.getCurrentSessionType() !== SESSION_TYPE.ADMIN) {
         alert("you are not admin, plaese sign in");
         $location.path('sign-in');
         // setTimeout("$('html').injector().get('$location').path('sign-in');$('html').scope().$apply();", 100);
     }
+
+    $scope.taskList = [
+
+    ];
+
+    
+
+
+
+
 });
