b53eed276e63f58353683a23843d4407d9a189ec
Olaf Ahn <adoji92@gmail.com>
2015-11-24 13:28:58 +0900

    Add NavActiveClassConstant

--- yofront/app/scripts/controllers/nav.js
+++ yofront/app/scripts/controllers/nav.js
@@ -1,17 +1,20 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name dbfrontappApp.controller:NavCtrl
- * @description
- * # NavCtrl
- * Controller of the dbfrontappApp
- */
+* @ngdoc function
+* @name dbfrontappApp.controller:NavCtrl
+* @description
+* # NavCtrl
+* Controller of the dbfrontappApp
+*/
 angular.module('dbfrontappApp')
-  .controller('NavCtrl', function () {
+.constant('NavbarActiveClass', 'active')
+.controller('NavCtrl', function (
+    $scope, $location,
+    NavbarActiveClass) {
     this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
     ];
-  });
+});
