a16f8f5a804273432ad44f85cee84debc7433d1b
Olaf Ahn <adoji92@gmail.com>
2015-11-24 19:50:42 +0900

    Reindet adminpage.js

--- yofront/app/scripts/controllers/adminpage.js
+++ yofront/app/scripts/controllers/adminpage.js
@@ -1,84 +1,84 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name dbfrontappApp.controller:AdminpageCtrl
- * @description
- * # AdminpageCtrl
- * Controller of the dbfrontappApp
- */
+* @ngdoc function
+* @name dbfrontappApp.controller:AdminpageCtrl
+* @description
+* # AdminpageCtrl
+* Controller of the dbfrontappApp
+*/
 angular.module('dbfrontappApp')
-  .controller('AdminpageCtrl', function ($scope, $location, SessionService, ApiService, SESSION_TYPE) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+.controller('AdminpageCtrl', function ($scope, $location, SessionService, ApiService, SESSION_TYPE) {
+  this.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Karma'
+  ];
 
-    // check admin!
-    SessionService.checkSessionType(SESSION_TYPE.ADMIN);
+  // check admin!
+  SessionService.checkSessionType(SESSION_TYPE.ADMIN);
 
-    $scope.taskList = [];
-    ApiService.getTaskList(function(res) {
-      console.log(res);
-        $scope.taskList = res.data;
-        console.log(res.data);
-    }, function(data) {
-      console.log('getTaskList error');
-        console.log(data);
-    });
-    console.log($scope.taskList);
-    // [{
-    //   id: 1,
-    //   name: 'name1',
-    //   description: 'description1',
-    //   minup: 'minup1',
-    //   tdtname: 'td1',
-    //   tdtschema: 'tds1',
-    // },{
-    //   id: 1,
-    //   name: 'name1',
-    //   description: 'description1',
-    //   minup: 'minup1',
-    //   tdtname: 'td1',
-    //   tdtschema: 'tds1',
-    // },{
-    //   id: 1,
-    //   name: 'name1',
-    //   description: 'description1',
-    //   minup: 'minup1',
-    //   tdtname: 'td1',
-    //   tdtschema: 'tds1',
-    // }];
-
-    $scope.userList = [{
-      id: 1,
-      str_id: 'id',
-      name: 'name',
-      sex: 'f',
-      address: 'adrs',
-      bdate: '5/6',
-      role: 'valuer',
-      score: 9.4
-    },{
-      id: 1,
-      str_id: 'id',
-      name: 'name',
-      sex: 'f',
-      address: 'adrs',
-      bdate: '5/6',
-      role: 'valuer',
-      score: 9.4
-    },{
-      id: 1,
-      str_id: 'id',
-      name: 'name',
-      sex: 'f',
-      address: 'adrs',
-      bdate: '5/6',
-      role: 'valuer',
-      score: 9.4
-    }];
-
-
+  $scope.taskList = [];
+  ApiService.getTaskList(function(res) {
+    console.log(res);
+    $scope.taskList = res.data;
+    console.log(res.data);
+  }, function(data) {
+    console.log('getTaskList error');
+    console.log(data);
   });
+  console.log($scope.taskList);
+  // [{
+  //   id: 1,
+  //   name: 'name1',
+  //   description: 'description1',
+  //   minup: 'minup1',
+  //   tdtname: 'td1',
+  //   tdtschema: 'tds1',
+  // },{
+  //   id: 1,
+  //   name: 'name1',
+  //   description: 'description1',
+  //   minup: 'minup1',
+  //   tdtname: 'td1',
+  //   tdtschema: 'tds1',
+  // },{
+  //   id: 1,
+  //   name: 'name1',
+  //   description: 'description1',
+  //   minup: 'minup1',
+  //   tdtname: 'td1',
+  //   tdtschema: 'tds1',
+  // }];
+
+  $scope.userList = [{
+    id: 1,
+    str_id: 'id',
+    name: 'name',
+    sex: 'f',
+    address: 'adrs',
+    bdate: '5/6',
+    role: 'valuer',
+    score: 9.4
+  },{
+    id: 1,
+    str_id: 'id',
+    name: 'name',
+    sex: 'f',
+    address: 'adrs',
+    bdate: '5/6',
+    role: 'valuer',
+    score: 9.4
+  },{
+    id: 1,
+    str_id: 'id',
+    name: 'name',
+    sex: 'f',
+    address: 'adrs',
+    bdate: '5/6',
+    role: 'valuer',
+    score: 9.4
+  }];
+
+
+});
