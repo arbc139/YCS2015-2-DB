55cf486c6112377d20aa550814e9d36b3bc61c6f
Olaf Ahn <adoji92@gmail.com>
2015-11-24 18:08:12 +0900

    Create new task view & ctrl

---
+++ yofront/app/scripts/controllers/newtask.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dbfrontappApp.controller:NewtaskCtrl
+ * @description
+ * # NewtaskCtrl
+ * Controller of the dbfrontappApp
+ */
+angular.module('dbfrontappApp')
+  .controller('NewtaskCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
