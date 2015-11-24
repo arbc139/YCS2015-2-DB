91fd133d7ccf967c7057120afc58e7a1e5494264
Olaf Ahn <adoji92@gmail.com>
2015-11-24 13:37:27 +0900

    Implement SessionService

--- yofront/app/scripts/services/sessionservice.js
+++ yofront/app/scripts/services/sessionservice.js
@@ -1,23 +1,50 @@
 'use strict';
 
 /**
- * @ngdoc service
- * @name dbfrontappApp.SessionService
- * @description
- * # SessionService
- * Factory in the dbfrontappApp.
- */
+* @ngdoc service
+* @name dbfrontappApp.SessionService
+* @description
+* # SessionService
+* Factory in the dbfrontappApp.
+*/
 angular.module('dbfrontappApp')
-  .factory('SessionService', function () {
-    // Service logic
-    // ...
+.constant('SESSION_TYPE', {
+    ADMIN: 'session_type_admin',
+    VALUER: 'session_type_valuer',
+    SUBMITTER: 'session_type_submitter',
+    WRONG: 'session_type_wrong'
+})
+.factory('SessionService', function () {
+  // Service logic
+  // ...
 
-    var meaningOfLife = 42;
+  var currentSession = {
+    id: -1,
+    str_id: '',
+    // type: SESSION_TYPE.WRONG
+    type: SESSION_TYPE.ADMIN
 
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
-    };
-  });
+  };
+
+  // Public API here
+  return {
+    getCurrentSessionType: function () {
+        return currentSession.type;
+    },
+    setCurrentSessionType: function(ss) {
+        currentSession.type = ss;
+    },
+    getStrId: function () {
+        return currentSession.str_id;
+    },
+    setStrId: function (si) {
+        currentSession.str_id = si;
+    },
+    getId: function () {
+        return currentSession.id;
+    },
+    setId: function(i) {
+        currentSession.id = i;
+    }
+  };
+});
