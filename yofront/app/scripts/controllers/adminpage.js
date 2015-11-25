2a813b4015d447648a58ed6eb8a32c653a8977ac
Olaf Ahn <adoji92@gmail.com>
2015-11-25 19:09:15 +0900

    Add Search function on id & name (not age, participating task name)

--- yofront/app/scripts/controllers/adminpage.js
+++ yofront/app/scripts/controllers/adminpage.js
@@ -27,15 +27,12 @@ angular.module('dbfrontappApp')
 
   ApiService.getUserList(function(res) {
     $scope.userList = res.data;
+    $scope.originalUserList = res.data;
   }, function(res) {
     console.log('getUserList error');
     console.log(res.data);
   });
 
-
-
-})
-.controller('AdminPageSearchCtrl', function($scope) {
   $scope.searchCategories = ["id", "role", "age", "task name"];
   $scope.selectedSearch = $scope.searchCategories[0];
 
@@ -43,5 +40,73 @@ angular.module('dbfrontappApp')
     $scope.selectedSearch = s;
   };
 
+  $scope.search = function(query) {
+    if (query === '') {
+      $scope.userList = $scope.originalUserList;
+      return;
+    }
+    if ($scope.selectedSearch === $scope.searchCategories[0]) {
+      // case 'id'
+
+      var tempArray = [];
+
+      for (var i in $scope.originalUserList) {
+        var user = $scope.originalUserList[i];
+
+        if (user.str_id === query) {
+          tempArray.push(user);
+        }
+      }
+
+      $scope.userList = tempArray;
+
+    } else if ($scope.selectedSearch === $scope.searchCategories[1]) {
+      // case 'role'
+
+      var tempArray = [];
+
+      for (var i in $scope.originalUserList) {
+        var user = $scope.originalUserList[i];
+
+        if (user.role === query) {
+          tempArray.push(user);
+        }
+      }
+
+      $scope.userList = tempArray;
+
+    } else if ($scope.selectedSearch === $scope.searchCategories[2]) {
+      //case 'age'
+
+      var tempArray = [];
+
+      for (var i in $scope.originalUserList) {
+        var user = $scope.originalUserList[i];
+
+        if (user.age === query) {
+          tempArray.push(user);
+        }
+      }
+
+      $scope.userList = tempArray;
+
+    } else if ($scope.selectedSearch === $scope.searchCategories[3]) {
+      // case 'task name'
+
+      var tempArray = [];
+
+      for (var i in $scope.originalUserList) {
+        var user = $scope.originalUserList[i];
+        if (user.str_id  === query) {
+          tempArray.push(user);
+        }
+      }
+
+      $scope.userList = tempArray;
+
+    } else {
+      alert('sibal?');
+    }
+  }
 
 });
