84138c59b1fbd2246d5718c7f8fbc897a329c5b5
Olaf Ahn <adoji92@gmail.com>
2015-11-26 14:35:43 +0900

    Create Submiitable Task

--- yofront/app/scripts/controllers/submitterpage.js
+++ yofront/app/scripts/controllers/submitterpage.js
@@ -18,14 +18,16 @@ angular.module('dbfrontappApp')
   // check submitter!
   SessionService.checkSessionType(SESSION_TYPE.SUBMITTER);
 
+
+  // begin appliable task
   ApiService.getTaskList(function(res) { // todo remove
-    $scope.taskList = res.data;
+    $scope.appliableTaskList = res.data;
   }, function(res) {
     console.log('getTaskList error');
     console.log(res.data);
   });
 
-  $scope.applyTask = function(tableId) {
+  $scope.apply = function(tableId) {
     alertify.confirm('waerjapweoirjapwoeirjapwoerijapwoe rjapwoeirj apwoeirj aopwer jawpeorj waoperijweoapjreowijriewjrowjrow jpaweorij wapoerijapwoeirj apoweirj pawoeirj apwoeirj apwoerij apweori jawpeori japweorij apweorij wpeo ijawpoerij awpoerij awpoerij oeirjweorjweojriewjr ojawperj awpeor jewioar jwaoierj apwei rjewaor jawpeoijr pawoerj poaewijr aoweirj apowirjawoer jpeor jaweporij apoewrij oej ioaewrj ajr opewjr oaej roaewjr aweirjpawrjpoeawijr paoeiwjr opaweijr',
     function(){
       alertify.success('Ok');
@@ -34,4 +36,28 @@ angular.module('dbfrontappApp')
       alertify.error('Cancel');
     });
   };
+  // end appliable task
+
+
+  // begin submittable task
+  ApiService.getTaskList(function(res) { // todo remove
+    $scope.submittableTask = res.data;
+  }, function(res) {
+    console.log('getTaskList error');
+    console.log(res.data);
+  });
+
+  $scope.submit = function(tableId) {
+
+  };
+
+  $scope.info = function(tableId) {
+
+  };
+
+  // end submittable task
+
+
+
+
 });
