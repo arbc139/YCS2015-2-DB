34e0ad87eac62a79f926d887165fd2fccd025dbe
Olaf Ahn <adoji92@gmail.com>
2015-11-27 13:46:40 +0900

    Create ApiService.newTask()

--- yofront/app/scripts/services/apiservice.js
+++ yofront/app/scripts/services/apiservice.js
@@ -33,6 +33,60 @@ angular.module('dbfrontappApp')
           }
         })
         .then(onS, onE);
+      },
+      newTask: function(params, onS, onE) {
+          var config = {
+            headers: {
+              'Accept': 'application/json',
+              'Content-type': 'application/json'
+            }
+          };
+
+          $http
+          .post('http://db.olaf.kr/api/login', params, config) // todo rename url
+          .then(onS, onE);
+          dfsdfsdf see todo
+          // .success(function (data) {
+          //   console.log('success ');
+          //   console.log(data);
+          //
+          //   var updateSession = function (si, i) {
+          //     console.log("updateSession: " + si + " , " + i);
+          //     SessionService.setStrId(si);
+          //     SessionService.setId(i);
+          //   };
+          //
+          //   if (data.role === 'admin') {
+          //     SessionService.setCurrentSessionType(SESSION_TYPE.ADMIN);
+          //     updateSession(data.str_id, data.id);
+          //
+          //     $location.path('admin-page');
+          //   } else if (data.role === 'valuer') {
+          //     SessionService.setCurrentSessionType(SESSION_TYPE.VALUER);
+          //     updateSession(data.str_id, data.id);
+          //
+          //     $location.path('valuer-page');
+          //   } else if (data.role === 'submitter') {
+          //     SessionService.setCurrentSessionType(SESSION_TYPE.SUBMITTER);
+          //     updateSession(data.str_id, data.id);
+          //
+          //     $location.path('submitter-page');
+          //   } else if (data.role === 'wrong') {
+          //     SessionService.setCurrentSessionType(SESSION_TYPE.WRONG);
+          //     alert('wrong id or pw\ntry again!');
+          //   } else {
+          //     alert('sibal??? ? ?sdfljasdlfjasdf');
+          //   }
+          // })
+          // .error(function (data) {
+          //   console.log('fail ' + data);
+          //   alert("server error!\nsee the debugging console");
+          // });
       }
+      // post '/api/admin/tasks’로 포스트 보내주면되고 argument는
+      // :t_name, :description, :minimum_upload_period, :task_data_table_name, :task_data_table_schema
+      // 이 순서대로
+      // task_data_table_schema는 그걸로보내줘
+      // column의 리스트?
     };
   });
