854c7d89516304d9024210bbed767b247bd9b4d0
Olaf Ahn <adoji92@gmail.com>
2015-11-30 18:14:05 +0900

    Add submitter file upload feature

--- yofront/app/scripts/controllers/submitter_task_submit.js
+++ yofront/app/scripts/controllers/submitter_task_submit.js
@@ -1,19 +1,31 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name dbfrontappApp.controller:SubmiiterSubmitCtrl
- * @description
- * # SubmiiterSubmitCtrl
- * Controller of the dbfrontappApp
- */
+* @ngdoc function
+* @name dbfrontappApp.controller:SubmiiterSubmitCtrl
+* @description
+* # SubmiiterSubmitCtrl
+* Controller of the dbfrontappApp
+*/
 angular.module('dbfrontappApp')
-  .controller('SubmitterTaskSubmitCtrl', function ($scope, $location) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+.controller('SubmitterTaskSubmitCtrl', function ($scope, $location) {
 
-    $scope.tableId = $location.search().tid;
-  });
+  $scope.tableId = $location.search().tid;
+
+  $scope.upload = function() {
+    var f = document.getElementById('file').files[0];
+    var r = new FileReader();
+    r.onloadend = function(e){
+      console.log(e);
+      $scope.data = e.target.result;
+      $("#btn-submit").prop('disabled', false);
+      $scope.$apply();
+    }
+    r.readAsText(f, 'UTF-8');
+    // r.readAsBinaryString(f);
+  }
+  $scope.submit = function() {
+    console.log($scope.data);
+    alertify.success($scope.data);
+  };
+});
