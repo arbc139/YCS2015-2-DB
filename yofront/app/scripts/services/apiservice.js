350e01f9faeab297d13a7e99f35eb831e7098fc2
Olaf Ahn <adoji92@gmail.com>
2015-12-01 13:42:16 +0900

    Implement getSubmitterTaskInfo @ ApiService

--- yofront/app/scripts/services/apiservice.js
+++ yofront/app/scripts/services/apiservice.js
@@ -200,6 +200,22 @@ angular.module('dbfrontappApp')
 
         $http.get(SERVER_URL + '/submitter/tasks/participate', params)
         .then(onS, onE);
+      },
+      getSubmitterTaskInfo: function(taskId) {
+        var uId = SessionService.getId();
+
+        if (uId === -1) {
+          alertify.error('user id -1 <br>(you should not use test session)');
+          return;
+        }
+
+        var params = {
+          user_id: uId,
+          task_id: taskId
+        };
+
+        $http.get(SERVER_URL + '/submitter/tasks/info', params)
+        .then(onS, onE);
       }
     };
   });
