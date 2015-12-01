66e9cb3e69bb3ba8879a4e4c3a43d622d082e65c
Olaf Ahn <adoji92@gmail.com>
2015-12-01 13:08:43 +0900

    Finish admin info (with redirect to /my-page-redirect)

--- yofront/app/scripts/controllers/admininfo.js
+++ yofront/app/scripts/controllers/admininfo.js
@@ -8,7 +8,7 @@
  * Controller of the dbfrontappApp
  */
 angular.module('dbfrontappApp')
-  .controller('AdmininfoCtrl', function (SessionService, SESSION_TYPE) {
+  .controller('AdmininfoCtrl', function ($scope, $location, ApiService, SessionService, SESSION_TYPE) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -17,5 +17,16 @@ angular.module('dbfrontappApp')
     // check admin!
     SessionService.checkSessionType(SESSION_TYPE.ADMIN);
 
-    
+    $scope.query = '';
+
+    $scope.change = function(query) {
+        ApiService.postModifyAdminPassword(query,
+      function(res) {
+        alertify.success('success');
+        $location.path('/my-page-redirect');
+      }, function() {
+        alertify.error('error');
+        console.log('error admin password change');
+      });
+    };
   });
