2deace37f6acb255f8124da39684192461a858b4
김도영 <DYKim@gimdoyeong-ui-MacBook-Pro.local>
2015-12-02 15:24:07 +0900

    PDSF model) bug fix

--- back/DBBack/app/models/parsing_data_sequence_file.rb
+++ back/DBBack/app/models/parsing_data_sequence_file.rb
@@ -36,7 +36,6 @@ class ParsingDataSequenceFile < ActiveRecord::Base
   # csv_file_col -> 원본데이터파일 string에서 얻은 attribute array
   # raw_schema_col -> 원본 데이터 schema 
   # tdt_schema_col -> 태스크 데이터 테이블 스키마
-​
   # TUPLES
   # csv_file_tuples -> 원본데이터파일의 tuples
   # raw_schema_tuples -> 원본 데이터 schema에 맞춰 뽑은 tuples
@@ -52,7 +51,7 @@ class ParsingDataSequenceFile < ActiveRecord::Base
     csv_file_tuples=[]
     raw_schema_tuples=[]
     tdt_tuples=[]
-    
+
     # input에서 attribute 이름만 뽑은 array
     csv_file_col = []
     raw_schema_col = raw_data_type_schema
@@ -68,13 +67,13 @@ class ParsingDataSequenceFile < ActiveRecord::Base
       tdt_schema_col << column[:col_name]
     end
     ​"""
-    
+
     #save csv_file_attribute
     tupleStr = CSV.parse(csv_file_string)
     for attribute in tupleStr[0]
       csv_file_col << attribute
     end
-    ​
+
     #Create csv_file_tuples & raw_schema_tuples 
     #raw_schema_tuples는 schema순서를 따르지 않음
     csv_file_tuples = CSV.parse(csv_file_string).map {|a| Hash[ csv_file_col.zip(a)]}
@@ -82,7 +81,7 @@ class ParsingDataSequenceFile < ActiveRecord::Base
     for tuple in csv_file_tuples
       raw_schema_tuples << tuple.select{|k,v| raw_schema_col.include?(k)}
     end
-    ​
+    
     #Create tdt_tuples except type
     for tuple in raw_schema_tuples
       temp = {}
@@ -92,16 +91,16 @@ class ParsingDataSequenceFile < ActiveRecord::Base
       end
       tdt_tuples << temp
     end
-    ​
+    
     # count duplicated_tuple_num
     duplicated_tuple = tdt_tuples.group_by {|tuple| tuple[tdt_schema_col[1]]}.values.select{|tdt_tuples| tdt_tuples.size>1}.flatten
-    ​
+    
     # Delete duplicated tuples
     tdt_tuples = tdt_tuples.uniq
-    ​
+    
     # null ratio check & tuples to CSV
     parsed_file << tdt_schema_col.join(",") << "\n"
-    ​
+    
     for tuple in tdt_tuples 
       for attribute in tdt_schema_col
         if tuple[attribute] == nil
@@ -110,11 +109,11 @@ class ParsingDataSequenceFile < ActiveRecord::Base
       end
         parsed_file<<tuple.values.join(",")<<"\n"
     end
-    ​
+    
     for attribute in tdt_schema_col
       col_null_ratios[attribute]/=tdt_tuples.length
     end
-    ​
+    
     returnHash['all_tuple_num'] = tdt_tuples.length
     returnHash['duplicated_tuple_num'] = duplicated_tuple.length
     returnHash['col_null_ratios'] = col_null_ratios
