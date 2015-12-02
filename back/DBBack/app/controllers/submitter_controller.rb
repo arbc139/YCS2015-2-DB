a8fc074f7c86620e1f6d0888b3693b81ecabbd90
김도영 <DYKim@gimdoyeong-ui-MacBook-Pro.local>
2015-12-02 15:43:11 +0900

    SUBMITTER) task submit implement, tiny bug fix

--- back/DBBack/app/controllers/submitter_controller.rb
+++ back/DBBack/app/controllers/submitter_controller.rb
@@ -132,7 +132,7 @@ class SubmitterController < ApplicationController
     # rdt_id (raw_data_type id)
     # period (회차)
     # inning (기간)
-    
+
     @csv = params[:csv]
     @submitter_id = params[:user_id]
     @valuer = User.get_random_valuer
@@ -140,13 +140,13 @@ class SubmitterController < ApplicationController
     @rdt = RawDataType.find(params[:rdt_id])
     @period = period
     @inning = inning
-
-    parse_result = parsing_file(params[:csv], rdt.schema, task.task_data_table_schema)
+    
+    parse_result = ParsingDataSequenceFile.parsing_file(params[:csv], rdt.schema, task.task_data_table_schema)
     # parse_result = parsing_file(params[:csv], rdt_schema, tdt_schema) # 영훈이의 파싱 함수 호출
     # return [:all_tuple_num], [:duplicated_tuple_num], [:col_null_ratios], [:parsed_file]
 
     @pdsf = ParsingDataSequenceFile.new(pdsf_params(parse_result, @period, @inning, @submitter_id, @valuer.id, @task.id, @rdt.id))
-
+    
     parse_result[:col_null_ratios].each do |col, null_ratio|
       ParseColumnNullRatio.create(
         column_name: col,
